7a2b5f02755709606f1618a88e86c6189689c394
Martín Díaz Bastida <181728462+martdbj@users.noreply.github.com>
2025-12-10 11:15:04 +0100

    Avance ejercicio Library

--- Tema4/FilesXML/library.php
+++ Tema4/FilesXML/library.php
@@ -8,54 +8,87 @@
 </head>
 
 <body>
-    <h1>Library</h1>
+<h1>Library</h1>
 
-    <?php
-    $booksLibrary = new DOMDocument();
-    $booksLibrary->load("biblioteca.xml");
+<?php
+$booksLibrary = new DOMDocument();
+$booksLibrary->load("biblioteca.xml");
 
-    // Lend boook
-    // Identify what books are not borrwoed to allow them to be selected
-    $lendBooks = fopen("lendBooks", "w+");
-    $finalLend = new DOMDocument();
+// Lend boook
+// Identify what books are not borrwoed to allow them to be selected
 
-    $books = $booksLibrary->getElementsByTagName("libro");
-    foreach ($books as $book) {
-        $status = $book->getElementsByTagName("estado");
-        foreach ($status as $stat) {
-            if ($stat->nodeValue == "libre") {
-                $finalLend->appendChild($book);
-            }
+$finalLend = new DOMDocument();
+
+$root = $finalLend->createElement('availableBooks');
+$finalLend->appendChild($root);
+
+$books = $booksLibrary->getElementsByTagName("libro");
+foreach ($books as $book) {
+    $status = $book->getElementsByTagName("estado");
+    foreach ($status as $stat) {
+        if ($stat->nodeValue == "libre") {
+            $availableBook = $finalLend->importNode($book, true);
+            $root->appendChild($availableBook);
         }
     }
+}
 
-    file_put_contents("lendBooks.xml", $finalLend->saveHTML());
-    ?>
+file_put_contents("lendBooks.xml", $finalLend->saveHTML());
+?>
 
-    <form action="library.php" method="post">
-        <select name="lendBooks[]" multiple>
+<form action="library.php" method="post">
+    <select name="lendBooks[]" multiple>
         <?php
+        $books = $booksLibrary->getElementsByTagName("libro");
         foreach ($books as $book) {
             $status = $book->getElementsByTagName("estado");
-            foreach ($book as $tag) {
+            foreach ($status as $stat) {
                 if ($stat->nodeValue == "libre") {
-                    $name = $book -> getElementsByTagName("name");
+                    $name = $book->getElementsByTagName("titulo");
                     foreach ($name as $nam) {
-                        echo '<option value="${$nam->nodeValue}">${nam->nodeValue}</option>';
+                        echo '<option value="' . $nam->nodeValue . '">' . $nam->nodeValue . '</option>';
                     }
-                    
                 }
             }
         }
         ?>
-        </select>
-        <button name="lendBooks">Lend</button>
-    </form>
+    </select>
+    <button name="lendBooksButton">Lend</button>
+</form>
 
 
-    <?php
-    ?>
+<?php
+if (isset($_POST['lendBooksButton'])) {
+    if (isset($_POST['lendBooks'])) {
+        $selected = $_POST['lendBooks'];
+        lendBook($selected);
+    }
+}
 
+?>
+
+<?php
+function lendBook($selectedBooks)
+{
+    $booksLibrary = new DOMDocument();
+    $booksLibrary->load("biblioteca.xml");
+
+    $date = new DateTime();
+    $date->modify('+2weeks');
+    $dateString = $date->format('d-m-Y');
+
+    $books = $booksLibrary->getElementsByTagName("libro");
+    foreach ($books as $book) {
+        $name = $book->getElementsByTagName("titulo");
+        foreach ($name as $nam) {
+            if (in_array($nam->nodeValue, $selectedBooks)) {
+                $book->appendChild($booksLibrary->createElement("fecha_devolucion", $dateString));
+            }
+        }
+    }
+    file_put_contents("biblioteca.xml", $booksLibrary->saveXML());
+}
+?>
 </body>
 
 </html>
